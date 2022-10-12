cf516f237d1c9fff67cd400272e8ac1c0a9d1871
Kittiporn Theamnooch <kittiporn132@gmail.com>
2022-10-12 11:02:38 +0700

    fix merge error

--- statistics.py
+++ statistics.py
@@ -1,8 +1,9 @@
 def average(data):
     """Return the average of a list of numeric values in data."""
-    if len(data)==0:
+    if len(data) == 0:
         raise ValueError("List must contain at least one value")
-    return sum(data)/len(data)
+    return sum(data) / len(data)
+
 
 def variance(data):
     """Return the population variance of a list of numbers in data.
@@ -32,13 +33,13 @@ def variance(data):
     4.0
     """
     # some deliberately misformatted code. Run flake8 and fix it.
-    n=len(data)
-    if n==0:
+    n = len(data)
+    if n == 0:
         raise ValueError("Must have at least one value")
-    average = sum(data)/n
-    return sum( [(x-average)**2 for x in data] )/n
+    avg = average(data)
+    return sum([(x - avg) ** 2 for x in data]) / n
+
 
 def stdev(data):
     """The population standard deviation of a list of data values."""
     return sqrt(variance(data))
-
